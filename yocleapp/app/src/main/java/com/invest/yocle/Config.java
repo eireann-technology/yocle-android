b54889a424be0f230c7bb288c14e61ce07692d23
alantypoon <alantypoon@gmail.com>
2018-06-14 16:34:01 +0800

    compilable on macos

--- yocleapp/app/src/main/java/com/invest/yocle/Config.java
+++ yocleapp/app/src/main/java/com/invest/yocle/Config.java
@@ -2,11 +2,16 @@ package com.invest.yocle;
 
 public interface Config {
 
-	static final String HTTP_SERVER_SHARE_ROOT = "http://m.adiai.com";
-	static final String HTTP_SERVER_ROOT = "http://m.adiai.com";
 //	static final String HTTPS_SERVER_ROOT = "https://videoboard.hk:8081/dev";
 //	static final String HTTPS_SERVER_ROOT = "https://dev.adiai.com:8441";
-	static final String HTTPS_SERVER_ROOT = "https://yolofolio2.cetl.hku.hk:18443/dev";
+
+	//static final String HTTP_SERVER_SHARE_ROOT = "http://m.adiai.com";
+	//static final String HTTP_SERVER_ROOT = "http://m.adiai.com";
+	//static final String HTTPS_SERVER_ROOT = "https://yolofolio2.cetl.hku.hk:18443/dev";
+
+	static final String HTTP_SERVER_SHARE_ROOT = "http://yocle.net";
+	static final String HTTP_SERVER_ROOT = "http://yocle.net";
+	static final String HTTPS_SERVER_ROOT = "https://yocle.net";
 
 	//static final String HOME_URL = HTTPS_SERVER_ROOT + "/anchortest.php";
 	static final String HOME_URL = HTTPS_SERVER_ROOT + "/index.php";
